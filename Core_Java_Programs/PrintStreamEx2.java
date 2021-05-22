b125135063701fff830ec9e86d045f1408327395
hdanao86 <hdanao86@gmail.com>
2021-05-22 19:05:06 +0530

    21 may sync block inter commication deadlock stringtokenizer

--- Core_Java_Programs/PrintStreamEx2.java
+++
@@ -1,8 +0,0 @@
-import java.io.PrintStream;
-
-public class PrintStreamEx2 {
-    public static void main(String[] args) {
-        int a=30;
-        System.out.printf("%d",a);//note : out is the object of printstream
-    }
-}
